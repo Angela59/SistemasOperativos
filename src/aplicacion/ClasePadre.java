279170b03e8c618e71a2d32199043e3cd042e91d
DAM <DAM@AULA1_16>
2013-04-26 12:39:35 +0200

    Proyecto inicial

--- src/aplicacion/ClasePadre.java
+++ src/aplicacion/ClasePadre.java
@@ -9,5 +9,20 @@ package aplicacion;
  * @author DAM
  */
 public class ClasePadre {
+    String version, ram, bits;
+
+    public ClasePadre(String version, String ram, String bits) {
+        this.version = version;
+        this.ram = ram;
+        this.bits = bits;
+    }
+    public ClasePadre(){
+        this.version = "Sin version";
+        this.ram = "Sin RAM";
+        this.bits = "Sin bits";
+    }
+    public String copiar (String copiar){
+        return "Se ha copiado el sistema operativo";        
+    }
     
 }
